51cb0be102840348839e60efd5189131da934535
audgeviolin07 <audgeviolin07@gmail.com>
2024-09-22 02:52:51 -0400

    pls work

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
-{
-    "rules": {
-      "@typescript-eslint/no-empty-object-type": "off"
-    }
-  }
-  
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'next/core-web-vitals',
+    ],
+    rules: {
+      '@typescript-eslint/no-empty-interface': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  };
