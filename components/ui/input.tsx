1270f4f177cbbce1c6effc17535b4dc3fde96931
audgeviolin07 <audgeviolin07@gmail.com>
2024-09-22 02:37:36 -0400

    input tsx again

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-// No need to declare an empty interface; use the type directly
-const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
