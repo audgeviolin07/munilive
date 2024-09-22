466b71207fa4e8610d77773c7a464beb141f081c
audgeviolin07 <audgeviolin07@gmail.com>
2024-09-22 11:48:16 -0400

    whoops

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           {/* munilogo.png image */}
           <Image src="/munilogo.png" alt="Muni Logo" width={450} height={450} />
           {/* Word "muni" under the image */}
-          <span className="text-2xl font-bold text-white">muni</span>
+          {/* <span className="text-2xl font-bold text-white">muni</span> */}
           <span className="text-2xl font-bold text-white"></span>
           {/* <span className="text-2xl font-bold text-center text-white"> on demand check-ins, safety and community for all</span> */}
         </div>
